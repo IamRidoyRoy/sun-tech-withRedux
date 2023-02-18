96592517be0dec1058be5341cdb75e3b34928d94
IamRidoyRoy <ridoyroycse53engg@gmail.com>
2023-02-18 10:36:39 +0600

    fetch data from products.json and shows it on homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import ProductCard from "../components/ProductCard";
 
 const Home = () => {
+
+  const [products, setProducts] = useState([]);
+  console.log(products);
+  useEffect(() => {
+    fetch('products.json')
+      .then(res => res.json())
+      .then(data => setProducts(data))
+  }, [])
+
+
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      <h1>This is home page</h1>
+    <div>
+      <h1 className="flex justify-center text-2xl">This is the home page</h1>
+
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
+        {
+          products.map((product) => <ProductCard key={product._id} product={product}></ProductCard>)
+        }
+      </div>
+
     </div>
   );
 };
