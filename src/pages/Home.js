9dc1de37815274b22240730d510016e2d601edb9
IamRidoyRoy <ridoyroycse53engg@gmail.com>
2023-02-23 12:03:03 +0600

    Add In Stock, AMD, Intel button on homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,16 +18,27 @@ const Home = () => {
   // const retrieveDataFromStore = useSelector((state) => state)
   // console.log(retrieveDataFromStore);
 
+  const activeClass = "text-white  bg-indigo-500 border-white";
   return (
-    <div>
-      <h1 className="flex justify-center text-2xl">This is the home page</h1>
-
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-        {
-          products.map((product) => <ProductCard key={product._id} product={product}></ProductCard>)
-        }
+    <div className='max-w-7xl gap-14 mx-auto my-10'>
+      <div className='mb-10 flex justify-end gap-5'>
+        <button
+          className={`border px-3 py-2 rounded-full font-semibold ${activeClass} `}
+        >
+          In Stock
+        </button>
+        <button className={`border px-3 py-2 rounded-full font-semibold`}>
+          AMD
+        </button>
+        <button className={`border px-3 py-2 rounded-full font-semibold`}>
+          Intel
+        </button>
       </div>
-
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-14'>
+        {products.map((product) => (
+          <ProductCard key={product.model} product={product} />
+        ))}
+      </div>
     </div>
   );
 };
